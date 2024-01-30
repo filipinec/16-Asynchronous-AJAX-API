ef8ad80502f04f233d98bd50f5da4f88c9980ae5
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-01-30 11:52:07 +0100

    Fetch API - Promise/Consume

--- starter/script.js
+++ starter/script.js
@@ -41,78 +41,68 @@ const countriesContainer = document.querySelector('.countries');
 
 // // Callback Hell
 
-// const renderCountry = function (data, className = '') {
-//   const html = `<article class="country ${className}">
-//   <img class="country__img" src="${data.flags.png}" />
-//   <div class="country__data">
-//     <h3 class="country__name">${data.name.common}</h3>
-//     <h4 class="country__region">${data.region}</h4>
-//     <p class="country__row"><span>👫</span>${(
-//       data.population / 1000000
-//     ).toFixed(2)}</p>
-//     <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
-//     <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
-//   </div>
-// </article>`;
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = '1';
-// };
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      data.population / 1000000
+    ).toFixed(2)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
 
-// const getCountryAndNeighbour = function (country) {
-//   // AJAX call country 1
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-//   request.send();
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-//   request.addEventListener('load', function () {
-//     console.log(this.responseText);
-//     const [data] = JSON.parse(this.responseText); //Convert to JSON
-//     console.log(data);
-//     //Render country
-//     renderCountry(data); // Call from above
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText); //Convert to JSON
+    console.log(data);
+    //Render country
+    renderCountry(data); // Call from above
 
-//     //Get neighbour countries
-//     const neighbours = data.borders;
+    //Get neighbour countries
+    const neighbours = data.borders;
 
-//     //If some country not have neighbour
-//     if (!neighbours) return;
+    //If some country not have neighbour
+    if (!neighbours) return;
 
-//     //forEach Method for looping trought array
-//     neighbours.forEach(cur => {
-//       //AJAX for all neighbours - search by country Code(Example: ALB)
-//       const request2 = new XMLHttpRequest();
-//       request2.open('GET', `https://restcountries.com/v3.1/alpha/${cur}`);
-//       request2.send();
-//       request2.addEventListener('load', function () {
-//         const [data2] = JSON.parse(this.responseText); //Convert to JSON
-//         console.log(data2);
-//         //Render country
-//         renderCountry(data2, 'neighbour'); // Call from above
-//       });
-//     });
-//   });
-// };
-// // Made card for more country
-// getCountryAndNeighbour('macedonia');
+    //forEach Method for looping trought array
+    neighbours.forEach(cur => {
+      //AJAX for all neighbours - search by country Code(Example: ALB)
+      const request2 = new XMLHttpRequest();
+      request2.open('GET', `https://restcountries.com/v3.1/alpha/${cur}`);
+      request2.send();
+      request2.addEventListener('load', function () {
+        const [data2] = JSON.parse(this.responseText); //Convert to JSON
+        console.log(data2);
+        //Render country
+        renderCountry(data2, 'neighbour'); // Call from above
+      });
+    });
+  });
+};
+// Made card for more country
+getCountryAndNeighbour('macedonia');
 
 ////////////////////////////////////////
 
 //  Fetch API - Promise/Consume
 
-// const getCountryData = function (country) {
-//   fetch(`https://restcountries.com/v3.1/name/${country}`)
-//     .then(response => response.json())
-//     .then(data => console.log(data));
-// };
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
 
-// getCountryData('macedonia');
-
-async function getCountry(country) {
-  const response = await axios({
-    method: 'GET',
-    url: `https://restcountries.com/v3.1/name/${country}`,
-  });
-  console.log(response);
-}
-
-getCountry('portugal');
+getCountryData('macedonia');
